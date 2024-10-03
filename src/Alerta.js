913920380ce467b69b81300b1cd41bff9094f3aa
ikev2023 <140184969+ikev2023@users.noreply.github.com>
2024-10-02 19:11:17 -0600

    Add files via upload

---
+++ src/Alerta.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const Alerta = () => {
+  
+  return (
+    <nav class="navbar bg-custom-black ">
+      <div class="container-fluid d-flex align-items-center justify-content-center">
+        <img src="https://dincantoshop.vercel.app/logos/logo_caballo.jpeg" alt="" class="img_logo2" />
+        <div class="d-flex align-items-center justify-content-center">
+          <h1 class="h1-footer p-3">Perfumeria de confianza</h1>
+        </div>
+      </div>
+    </nav>
+  );
+};
---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import "./App.css";
+import { Alerta } from "./Alerta";
+function App() {
+  return (
+    <div>
+          <Alerta />
+    </div>
+  );
+}
+
+export default App;
